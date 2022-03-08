8c13139661ae0a1d94f0fc0fc4105c9e5998759b
EnterpriseJavaProject <85710578+EnterpriseJavaProject@users.noreply.github.com>
2022-03-08 16:06:41 +0000

    Rename course to course_id on student result table

--- automated_student_results_system.sql
+++ automated_student_results_system.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 01, 2022 at 02:37 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 7.3.28
+-- Generation Time: Mar 08, 2022 at 05:04 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.3.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -44,7 +44,7 @@ CREATE TABLE `courses` (
 --
 
 INSERT INTO `courses` (`id`, `course_name`, `course_level`, `status`, `created_at`, `updated_at`, `course_start_date`, `course_end_date`, `certificate_issuedate`) VALUES
-(1, 'CERTIFICATE IN SOFTWARE DEVELOPMENT', 'CSD1.1', 'Active', '2020-10-15 23:53:42', '2020-10-15 23:54:30', '2020-09-29', '2021-10-10', '2020-10-15'),
+(1, 'CERTIFICATE IN SOFTWARE DEVELOPMENT', 'CSD1.1', 'Active', '2020-10-15 23:53:42', '2022-03-08 10:29:28', '2020-09-08', '2021-10-10', '2020-10-15'),
 (2, 'INTERNATIONAL COMPUTER DRIVING LICENCE', 'ICDL4.2', 'Active', '2020-10-16 00:03:59', '2021-09-28 11:49:41', '2021-09-27', '2022-10-20', '2021-09-28'),
 (3, 'CERTIFICATE IN SOFTWARE DEVELOPMENT', 'CSD4.2', 'Active', '2020-10-16 00:06:43', '2020-10-16 00:06:43', '2021-10-18', '2022-03-15', '2020-10-16'),
 (4, 'CISCO CERTIFIED NETWORK ASSOCIATE', 'CCNA9.2', 'Active', '2021-09-28 11:15:49', '2021-10-20 13:49:47', '2021-03-15', '2022-11-20', '2021-09-28'),
@@ -66,6 +66,8 @@ CREATE TABLE `modules` (
   `staff_name` varchar(255) DEFAULT NULL,
   `course_id` int(250) DEFAULT NULL,
   `status` varchar(255) DEFAULT NULL,
+  `module_start_date` date DEFAULT NULL,
+  `module_end_date` date DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -74,25 +76,25 @@ CREATE TABLE `modules` (
 -- Dumping data for table `modules`
 --
 
-INSERT INTO `modules` (`id`, `module_name`, `course_name`, `staff_name`, `course_id`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'HTML/CSS', 'CERTIFICATE IN SOFTWARE DEVELOPMENT---CSD1.1', 'Mr Francis Korsah', 1, 'Active', '2020-10-16 00:08:37', '2021-09-27 16:14:50'),
-(2, 'Oracle Database', 'CERTIFICATE IN SOFTWARE DEVELOPMENT---CSD4.2', 'Bismark Otu', 3, 'Active', '2020-10-16 00:09:34', '2021-09-27 16:15:32'),
-(3, 'Microsoft', 'INTERNATIONAL COMPUTER DRIVING LICENCE---ICDL4.2', 'Mr Kennedy Asewie', 2, 'Active', '2020-10-16 00:13:11', '2021-10-19 09:56:46'),
-(4, 'Oracle Database', 'CERTIFICATE IN SOFTWARE DEVELOPMENT---CSD1.1', 'Bismark Otu', 1, 'Active', '2020-10-16 00:15:10', '2021-09-27 16:15:39'),
-(5, 'HTML/CSS', 'CERTIFICATE IN SOFTWARE DEVELOPMENT---CSD4.2', 'Mr Francis Korsah', 3, 'Active', '2020-10-16 00:17:59', '2021-09-27 16:15:55'),
-(6, 'Software Engineering', 'CERTIFICATE IN SOFTWARE DEVELOPMENT---CSD1.1', 'Mr Kennedy Asewie', 1, 'Active', '2020-10-16 00:19:53', '2021-09-27 16:15:43'),
-(7, 'Software Engineering', 'CERTIFICATE IN SOFTWARE DEVELOPMENT---CSD4.2', 'Mr Kennedy Asewie', 3, 'Active', '2020-10-16 00:20:48', '2021-09-27 16:15:49'),
-(8, 'JavaScript', 'CERTIFICATE IN SOFTWARE DEVELOPMENT---CSD1.1', 'Mr Francis Korsah', 1, 'Active', '2021-10-18 15:37:33', '2021-10-18 15:37:33'),
-(9, 'Excel', 'INTERNATIONAL COMPUTER DRIVING LICENCE---ICDL4.2', 'Nana Kwesi', 2, 'Active', '2021-09-28 11:11:19', '2021-10-19 09:56:55'),
-(10, 'Networking', 'CISCO CERTIFIED NETWORK ASSOCIATE---CCNA9.2', 'Nana Kwesi', 4, 'Active', '2021-09-28 11:16:28', '2021-10-14 14:36:32'),
-(11, 'Fundamentals of Computing', 'CERTIFICATE IN SOFTWARE DEVELOPMENT---CSD1.1', 'Mr Francis Korsah', 1, 'Active', '2021-09-28 11:20:15', '2021-10-14 14:37:47'),
-(12, 'Maven', 'JAVA ENTERPRISE---JAVA7.2', 'Mr Francis Korsah', 5, 'Active', '2021-09-28 11:22:20', '2021-10-19 10:01:04'),
-(13, 'Router Configuration', 'CISCO CERTIFIED NETWORK ASSOCIATE---CCNA9.2', 'Bismark Otu', 4, 'Active', '2021-09-28 11:27:10', '2021-10-14 14:36:53'),
-(14, 'Hibernate', 'JAVA ENTERPRISE---JAVA7.2', 'Mr Yao', 5, 'Active', '2021-09-28 11:34:30', '2021-10-19 10:04:01'),
-(15, 'Python', 'CERTIFICATE IN SOFTWARE DEVELOPMENT---CSD1.1', 'Elvis Segbewu', 1, 'Active', '2021-09-28 11:39:14', '2021-10-18 13:41:59'),
-(16, 'Powerpoint', 'INTERNATIONAL COMPUTER DRIVING LICENCE---ICDL4.2', 'Mr Kennedy Asewie', 2, 'Active', '2021-09-28 11:43:56', '2021-10-19 09:57:05'),
-(18, 'CORE JAVA & JDBC', 'DIPLOMA IN BUSINESS COMPUTING---DBC1.1', 'Mr Francis Korsah', 6, 'Active', '2021-10-20 14:32:31', '2022-02-07 10:03:53'),
-(19, 'PHP', 'DIPLOMA IN BUSINESS COMPUTING', 'Mr Edward Yeboah', 6, 'Active', '2021-10-20 16:12:34', '2022-02-07 10:03:43');
+INSERT INTO `modules` (`id`, `module_name`, `course_name`, `staff_name`, `course_id`, `status`, `module_start_date`, `module_end_date`, `created_at`, `updated_at`) VALUES
+(1, 'HTML/CSS', 'CERTIFICATE IN SOFTWARE DEVELOPMENT---CSD1.1', 'Mr Francis Korsah', 1, 'Active', '2020-09-08', '2022-10-20', '2020-10-16 00:08:37', '2022-03-08 10:32:11'),
+(2, 'Oracle Database', 'CERTIFICATE IN SOFTWARE DEVELOPMENT---CSD4.2', 'Bismark Otu', 3, 'Active', '2020-09-08', '2022-10-20', '2020-10-16 00:09:34', '2022-03-08 10:32:14'),
+(3, 'Microsoft', 'INTERNATIONAL COMPUTER DRIVING LICENCE---ICDL4.2', 'Mr Kennedy Asewie', 2, 'Active', '2020-09-08', '2022-10-20', '2020-10-16 00:13:11', '2022-03-08 10:32:18'),
+(4, 'Oracle Database', 'CERTIFICATE IN SOFTWARE DEVELOPMENT---CSD1.1', 'Bismark Otu', 1, 'Active', '2020-09-08', '2022-10-20', '2020-10-16 00:15:10', '2022-03-08 10:32:20'),
+(5, 'HTML/CSS', 'CERTIFICATE IN SOFTWARE DEVELOPMENT---CSD4.2', 'Mr Francis Korsah', 3, 'Active', '2020-09-08', '2022-10-20', '2020-10-16 00:17:59', '2022-03-08 10:32:22'),
+(6, 'Software Engineering', 'CERTIFICATE IN SOFTWARE DEVELOPMENT---CSD1.1', 'Mr Kennedy Asewie', 1, 'Active', '2020-09-08', '2022-10-20', '2020-10-16 00:19:53', '2022-03-08 10:32:25'),
+(7, 'Software Engineering', 'CERTIFICATE IN SOFTWARE DEVELOPMENT---CSD4.2', 'Mr Kennedy Asewie', 3, 'Active', NULL, '2022-10-20', '2020-10-16 00:20:48', '2022-03-08 10:32:29'),
+(8, 'JavaScript', 'CERTIFICATE IN SOFTWARE DEVELOPMENT---CSD1.1', 'Mr Francis Korsah', 1, 'Active', '2020-09-08', '2022-10-20', '2021-10-18 15:37:33', '2022-03-08 10:32:31'),
+(9, 'Excel', 'INTERNATIONAL COMPUTER DRIVING LICENCE---ICDL4.2', 'Nana Kwesi', 2, 'Active', '2020-09-08', '2022-10-20', '2021-09-28 11:11:19', '2022-03-08 10:32:33'),
+(10, 'Networking', 'CISCO CERTIFIED NETWORK ASSOCIATE---CCNA9.2', 'Nana Kwesi', 4, 'Active', '2020-09-08', '2022-10-20', '2021-09-28 11:16:28', '2022-03-08 10:32:35'),
+(11, 'Fundamentals of Computing', 'CERTIFICATE IN SOFTWARE DEVELOPMENT---CSD1.1', 'Mr Francis Korsah', 1, 'Active', '2020-09-08', '2022-10-20', '2021-09-28 11:20:15', '2022-03-08 10:32:38'),
+(12, 'Maven', 'JAVA ENTERPRISE---JAVA7.2', 'Mr Francis Korsah', 5, 'Active', '2020-09-08', '2022-10-20', '2021-09-28 11:22:20', '2022-03-08 10:32:39'),
+(13, 'Router Configuration', 'CISCO CERTIFIED NETWORK ASSOCIATE---CCNA9.2', 'Bismark Otu', 4, 'Active', '2020-09-08', '2022-10-20', '2021-09-28 11:27:10', '2022-03-08 10:32:41'),
+(14, 'Hibernate', 'JAVA ENTERPRISE---JAVA7.2', 'Mr Yao', 5, 'Active', '2020-09-08', '2022-10-20', '2021-09-28 11:34:30', '2022-03-08 10:32:43'),
+(15, 'Python', 'CERTIFICATE IN SOFTWARE DEVELOPMENT---CSD1.1', 'Elvis Segbewu', 1, 'Active', '2020-09-08', '2022-10-20', '2021-09-28 11:39:14', '2022-03-08 10:32:47'),
+(16, 'Powerpoint', 'INTERNATIONAL COMPUTER DRIVING LICENCE---ICDL4.2', 'Mr Kennedy Asewie', 2, 'Active', '2020-09-08', '2022-10-20', '2021-09-28 11:43:56', '2022-03-08 10:32:49'),
+(18, 'CORE JAVA & JDBC', 'DIPLOMA IN BUSINESS COMPUTING---DBC1.1', 'Mr Francis Korsah', 6, 'Active', '2020-09-08', '2022-10-20', '2021-10-20 14:32:31', '2022-03-08 10:32:51'),
+(19, 'PHP', 'DIPLOMA IN BUSINESS COMPUTING', 'Mr Edward Yeboah', 6, 'Active', '2020-09-08', '2022-10-20', '2021-10-20 16:12:34', '2022-03-08 10:32:53');
 
 -- --------------------------------------------------------
 
@@ -107,7 +109,7 @@ CREATE TABLE `staffs` (
   `email` varchar(100) DEFAULT NULL,
   `contact` varchar(250) DEFAULT NULL,
   `usertype` varchar(255) DEFAULT NULL,
-  `course` varchar(255) DEFAULT NULL,
+  `course_id` int(255) DEFAULT NULL,
   `status` varchar(20) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
@@ -117,20 +119,20 @@ CREATE TABLE `staffs` (
 -- Dumping data for table `staffs`
 --
 
-INSERT INTO `staffs` (`id`, `name`, `staff_id`, `email`, `contact`, `usertype`, `course`, `status`, `created_at`, `updated_at`) VALUES
-(1, 'Mr Francis Korsah', 'St2632', 'firstinstructor@aiti-kace.com.gh', '0555560810', 'Instructor', 'CERTIFICATE IN SOFTWARE DEVELOPMENT', 'Active', '2021-09-18 08:55:07', '2021-09-22 11:28:20'),
+INSERT INTO `staffs` (`id`, `name`, `staff_id`, `email`, `contact`, `usertype`, `course_id`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'Mr Francis Korsah', 'St2632', 'firstinstructor@aiti-kace.com.gh', '0555560810', 'Instructor', 1, 'Active', '2021-09-18 08:55:07', '2022-03-08 11:53:06'),
 (2, 'Mr Lawrence Kpodo', 'St6833', 'academicsecretary@aiti-kace.com.gh', '0547386724', 'Academic Secretary', NULL, 'Active', '2021-09-18 08:50:25', '2021-10-20 15:25:22'),
 (3, 'Mr Gabriel Dwamena', 'St3073', 'coursecordinator@aiti-kace.com.gh', '0207638264', 'Course Cordinator', NULL, 'Active', '2021-09-18 08:51:50', '2021-10-20 15:25:14'),
 (4, 'Dr Yaw Okraku-Yirenkyi', 'St9462', 'directorofstudies@aiti-kace.com.gh', '0207693568', 'Director of Studies', NULL, 'Active', '2021-09-18 09:04:03', '2021-10-20 17:08:19'),
-(5, 'Kennedy Asewie', 'St7308', 'secondinstructor@aiti-kace.com.gh', '0542426541', 'Instructor', 'INTERNATIONAL COMPUTER DRIVING LICENCE', 'Active', '2021-09-18 09:19:34', '2022-02-15 11:40:20'),
-(6, 'Mr Yao', 'ST7834', 'yaoa@aiti-kace.com.gh', '0207457634', 'Instructor', 'JAVA ENTERPRISE', 'Active', '2021-10-19 10:03:24', '2021-10-19 10:03:24'),
-(7, 'Bismark Otu', 'ST41941', 'bismarko@aiti-kace.com.gh', '0549057688', 'Instructor', 'CERTIFICATE IN SOFTWARE DEVELOPMENT', 'Active', '2021-09-24 10:47:14', '2021-09-24 10:47:14'),
-(8, 'Nana Kwesi', 'St845', 'nanakwesi1006@hotmail.com', '0247016266', 'Instructor', 'International Computer Driving Licence', 'Active', '2021-09-24 10:49:08', '2021-09-24 10:52:25'),
-(9, 'Mr Hermas Songtaa', 'ST5768', 'hermass@aiti-kace.com.gh', '0548276444', 'Instructor', 'CERTIFICATE IN SOFTWARE DEVELOPMENT', 'Active', '2021-10-20 14:06:27', '2021-10-20 14:06:27'),
-(10, 'Elvis Segbawu', 'ST5645', 'elviss@aiti-kace.com.gh', '0505736458', 'Instructor', 'CERTIFICATE IN SOFTWARE DEVELOPMENT	', 'Active', '2021-10-20 16:00:15', '2021-10-20 16:02:19'),
-(11, 'Jude Clottey', 'ST7429', 'judec@aiti-kace.com.gh', '0275856453', 'Instructor', 'CERTIFICATE IN SOFTWARE DEVELOPMENT', 'Active', '2021-10-20 16:09:35', '2021-10-20 16:09:35'),
-(12, 'Mr Edward Yeboah', 'ST6580', 'edwardy@aiti-kace.com.gh', '0249658903', 'Instructor', 'DIPLOMA IN BUSINESS COMPUTING', 'Active', '2021-10-20 16:12:11', '2021-10-20 16:12:11'),
-(13, 'Mr Isaac', 'ST8920', 'isaaca@aiti-kace.com.gh', '0573856354', 'Instructor', 'NETWORK INFORMATION SECURITY', 'Active', '2021-10-21 16:49:37', '2021-10-21 16:49:37');
+(5, 'Kennedy Asewie', 'St7308', 'secondinstructor@aiti-kace.com.gh', '0542426541', 'Instructor', 2, 'Active', '2021-09-18 09:19:34', '2022-03-08 11:53:22'),
+(6, 'Mr Yao', 'ST7834', 'yaoa@aiti-kace.com.gh', '0207457634', 'Instructor', 5, 'Active', '2021-10-19 10:03:24', '2022-03-08 11:53:30'),
+(7, 'Bismark Otu', 'ST41941', 'bismarko@aiti-kace.com.gh', '0549057688', 'Instructor', 3, 'Active', '2021-09-24 10:47:14', '2022-03-08 11:54:03'),
+(8, 'Nana Kwesi', 'St845', 'nanakwesi1006@hotmail.com', '0247016266', 'Instructor', 2, 'Active', '2021-09-24 10:49:08', '2022-03-08 11:54:09'),
+(9, 'Mr Hermas Songtaa', 'ST5768', 'hermass@aiti-kace.com.gh', '0548276444', 'Instructor', 1, 'Active', '2021-10-20 14:06:27', '2022-03-08 11:54:14'),
+(10, 'Elvis Segbawu', 'ST5645', 'elviss@aiti-kace.com.gh', '0505736458', 'Instructor', 3, 'Active', '2021-10-20 16:00:15', '2022-03-08 11:54:19'),
+(11, 'Jude Clottey', 'ST7429', 'judec@aiti-kace.com.gh', '0275856453', 'Instructor', 1, 'Active', '2021-10-20 16:09:35', '2022-03-08 11:54:23'),
+(12, 'Mr Edward Yeboah', 'ST6580', 'edwardy@aiti-kace.com.gh', '0249658903', 'Instructor', 6, 'Active', '2021-10-20 16:12:11', '2022-03-08 11:54:34'),
+(13, 'Mr Isaac', 'ST8920', 'isaaca@aiti-kace.com.gh', '0573856354', 'Instructor', 7, 'Active', '2021-10-21 16:49:37', '2022-03-08 11:54:41');
 
 -- --------------------------------------------------------
 
@@ -238,7 +240,8 @@ ALTER TABLE `modules`
 -- Indexes for table `staffs`
 --
 ALTER TABLE `staffs`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cou_fk` (`course_id`);
 
 --
 -- Indexes for table `students`
@@ -299,6 +302,12 @@ ALTER TABLE `students_results`
 --
 ALTER TABLE `modules`
   ADD CONSTRAINT `fk_course_id` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`);
+
+--
+-- Constraints for table `staffs`
+--
+ALTER TABLE `staffs`
+  ADD CONSTRAINT `cou_fk` FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`);
 
 --
 -- Constraints for table `students`
