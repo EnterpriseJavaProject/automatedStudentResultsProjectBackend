e711ad668fde26eb4478fb21280e5a0b1bf33ade
EnterpriseJavaProject <85710578+EnterpriseJavaProject@users.noreply.github.com>
2022-07-14 11:14:36 +0000

    Updates

--- src/main/java/com/aiti/java/project/repository/CoursesRepository.java
+++ src/main/java/com/aiti/java/project/repository/CoursesRepository.java
@@ -10,27 +10,33 @@ import com.aiti.java.project.entities.Courses;
 public interface CoursesRepository extends JpaRepository<Courses, Long> {
 
 	
+	// SELECTING FROM COURSE TABLE FOR RESULTS
 	
-		// SELECTING FROM COURSE TABLE FOR RESULTS
-	
-	@Query(value="SELECT DISTINCT courses.id, courses.course_name, courses.course_level, courses.status, courses.created_at, courses.updated_at, courses.course_start_date, courses.course_end_date, courses.certificate_issuedate FROM courses inner join students_results on courses.id = students_results.course_id where courses.id = students_results.course_id and students_results.student_id = ? and students_results.course_id = ?  GROUP by courses.id ORDER BY courses.id", nativeQuery=true)
-	public List<Courses> selectingFromCourseForResults(String student_id, int course_id);
-	
-	
-	
+@Query(value="SELECT courses.id, courses.coordinator, courses.course_name, courses.course_level, courses.status, courses.created_at, courses.updated_at, courses.course_start_date, courses.course_end_date, courses.certificate_issuedate FROM courses inner join students_results on courses.id = students_results.course_id where courses.id = students_results.course_id and students_results.student_id = ? and students_results.course_id = ?  GROUP by courses.id ORDER BY courses.id", nativeQuery=true)
+public List<Courses> selectingFromCourseForResults(String student_id, int course_id);
+
 	
 	
 	//	FIND STUDENT COURSES
 	
-	@Query(value="SELECT courses.id, courses.course_name, courses.course_level, courses.status, courses.created_at, courses.updated_at, courses.course_start_date, courses.course_end_date, courses.certificate_issuedate FROM courses inner join students on courses.id = students.course_id WHERE courses.id = students.course_id and students.id = ? GROUP by courses.course_name order by courses.course_name", nativeQuery=true)
+	@Query(value="SELECT courses.id, courses.coordinator, courses.course_name, courses.course_level, courses.status, courses.created_at, courses.updated_at, courses.course_start_date, courses.course_end_date, courses.certificate_issuedate FROM courses inner join students on courses.id = students.course_id WHERE courses.id = students.course_id and students.id = ? GROUP by courses.course_name order by courses.course_name", nativeQuery=true)
 	public List<Courses> findStudentCourses(Long student_id);
+	
+	
+	
+	
+	
+	//	FIND PAST STUDENT COURSES
+	
+	@Query(value="SELECT courses.id, courses.coordinator, courses.course_name, courses.course_level, courses.status, courses.created_at, courses.updated_at, courses.course_start_date, courses.course_end_date, courses.certificate_issuedate FROM courses inner join past_students on courses.id = past_students.course_id WHERE courses.id = past_students.course_id and past_students.id = ? GROUP by courses.course_name order by courses.course_name", nativeQuery=true)
+	public List<Courses> findPastStudentCourses(Long student_id);
 	
 	
 	
 	
 //	FIND STAFF COURSES
 	
-	@Query(value="SELECT courses.id, courses.course_name, courses.course_level, courses.status, courses.created_at, courses.updated_at, courses.course_start_date, courses.course_end_date, courses.certificate_issuedate FROM courses inner join staffs on courses.id = staffs.course_id WHERE courses.id = staffs.course_id and staffs.id = ? GROUP by courses.course_name order by courses.course_name", nativeQuery=true)
+	@Query(value="SELECT courses.id, courses.coordinator, courses.course_name, courses.course_level, courses.status, courses.created_at, courses.updated_at, courses.course_start_date, courses.course_end_date, courses.certificate_issuedate FROM courses inner join staffs on courses.id = staffs.course_id WHERE courses.id = staffs.course_id and staffs.id = ? GROUP by courses.course_name order by courses.course_name", nativeQuery=true)
 	public List<Courses> findStaffCourses(Long staff_id);
 	
 	
@@ -39,7 +45,7 @@ public interface CoursesRepository extends JpaRepository<Courses, Long> {
 	
 	
 	// FIND COURSE BY COURSE NAME
-	@Query(value="select id,course_name,course_level,status,created_at,updated_at,course_start_date,course_end_date,certificate_issuedate from courses where courses.course_name = ? order by id" , nativeQuery = true )
+	@Query(value="select id,course_name,coordinator,course_level,status,created_at,updated_at,course_start_date,course_end_date,certificate_issuedate from courses where courses.course_name = ? order by id" , nativeQuery = true )
 	List<Courses> findCourseByCourseName(String course_name);
 	
 	
@@ -47,7 +53,7 @@ public interface CoursesRepository extends JpaRepository<Courses, Long> {
 	
 	
 	// FIND COURSE BY COURSE LEVEL
-	@Query(value="select id,course_name,course_level,status,created_at,updated_at,course_start_date,course_end_date,certificate_issuedate from courses where courses.course_level = ? order by id" , nativeQuery = true )
+	@Query(value="select id,course_name,coordinator,course_level,status,created_at,updated_at,course_start_date,course_end_date,certificate_issuedate from courses where courses.course_level = ? order by id" , nativeQuery = true )
 	List<Courses> findCourseByCourseLevel(String course_level);
 	
 	
@@ -70,7 +76,7 @@ public interface CoursesRepository extends JpaRepository<Courses, Long> {
 	
 	
 	
-	@Query(value="select id,course_name,course_level,staff_name,status,created_at,updated_at,certificate_issuedate,course_start_date,course_end_date from courses order by id;", nativeQuery=true)
+	@Query(value="select id,course_name,coordinator,course_level,staff_name,status,created_at,updated_at,certificate_issuedate,course_start_date,course_end_date from courses order by id;", nativeQuery=true)
 	public List<Courses> findAllCourses();
 	
 	
