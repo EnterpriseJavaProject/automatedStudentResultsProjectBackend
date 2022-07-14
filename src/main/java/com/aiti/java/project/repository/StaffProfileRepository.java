5ed83f01bb824280c975a569fd12634ab1b31d47
EnterpriseJavaProject <85710578+EnterpriseJavaProject@users.noreply.github.com>
2022-07-14 11:16:58 +0000

    Updates

--- src/main/java/com/aiti/java/project/repository/StaffProfileRepository.java
+++ src/main/java/com/aiti/java/project/repository/StaffProfileRepository.java
@@ -9,10 +9,19 @@ import com.aiti.java.project.entities.Staffs;
 
 public interface StaffProfileRepository extends JpaRepository<Staffs, Integer> {
 	
+
+
+	
+	
+	// SELECTING ONLY EMAIL FORM STAFF AND SENDING THEM EMAIL NOTIFICATIONS
+	@Query(value="SELECT staffs.email FROM staffs inner join modules on staffs.course_id = modules.course_id and staffs.name = modules.staff_name where staffs.course_id = modules.course_id and staffs.name = modules.staff_name GROUP by modules.updated_at order by modules.updated_at DESC limit 1" , nativeQuery = true )
+	String selectingStaffEmailtoSendEmailNotification();
+	
+	
+	
 //  USERS LOGIN ROLE
 	@Query(value="SELECT DISTINCT staffs.id, staffs.name, staffs.staff_id, staffs.email, staffs.contact, staffs.usertype, staffs.course_id, staffs.status, staffs.created_at, staffs.updated_at from staffs INNER join users on staffs.staff_id = users.staff_id and staffs.email = users.email and staffs.usertype = users.usertype where staffs.staff_id = users.staff_id and staffs.email = users.email and staffs.usertype = users.usertype and users.email = ? and users.password = ? " , nativeQuery = true )
 	List<Staffs> userLoginRole(String email, String password);
-	
 	
 	
 	
@@ -45,18 +54,28 @@ public interface StaffProfileRepository extends JpaRepository<Staffs, Integer> {
 	@Query(value="select count(*) from staffs", nativeQuery=true)
 	public String countStaff();
 	
-	
-	
-	
-	
-	
-	
-	
+
 	
 	
 	// RETRIEVE ONLY STAFF NAME 
 	
 	@Query(value="select staffs.name from staffs where staffs.course_id = ?", nativeQuery=true)
 	public Staffs retrieveStaffName(int id);
+	
+
+	
+	
+	
+	
+	
+	// FIND STAFF BY ID
+	
+	@Query(value="SELECT `id`, `name`, `staff_id`, `email`, `contact`, `usertype`, `course_id`, `status`, `created_at`, `updated_at` FROM `staffs` WHERE staffs.id = ?", nativeQuery=true)
+	public List<Object> findStaffById(int id);
+	
+	
+	
+	
+	
 	
 }
